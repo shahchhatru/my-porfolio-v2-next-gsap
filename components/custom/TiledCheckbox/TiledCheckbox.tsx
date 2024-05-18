b2eeccfc791ecba389ee2e3ea6cb82be03d80ae5
shahchhatru <shahkhesavchhatra@gmail.com>
2024-05-18 13:59:13 +0545

    TiledCheckbox made much more good looking'

--- components/custom/TiledCheckbox/TiledCheckbox.tsx
+++ components/custom/TiledCheckbox/TiledCheckbox.tsx
@@ -1,5 +1,5 @@
-'use client';
-import React from "react";
+'use client'
+import React, { useState } from "react";
 import { Icon } from '@iconify-icon/react';
 
 interface TiledCheckboxProps {
@@ -9,14 +9,27 @@ interface TiledCheckboxProps {
 }
 
 function TiledCheckbox({ id, name, icon }: TiledCheckboxProps) {
+    const [isChecked, setIsChecked] = useState(false);
+
+    const handleOnClick = () => {
+        setIsChecked(!isChecked);
+        console.log(`${name} checkbox is checked: ${!isChecked}`);
+    };
+
     return (
-        <div className="tile relative cursor-pointer group py-8">
-            <input type="checkbox" name={name} id={id} className="absolute h-full w-full  rounded-2xl cursor-pointer appearance-none "  />
-            <label htmlFor={id} className="flex flex-col items-center justify-center gap-4 h-80% w-full absolute bottom-0 cursor-pointer">
-                <Icon icon={icon} width="64" height="64" className="text-black transition-border p-2 duration-200 ease-in-out group-hover:border-green-500 group-hover:border-2 group-hover:rounded-md" />
+        <div className={`tile grid grid-cols-1 grid-rows-4 cursor-pointer group `}>
+            <input
+                type="checkbox"
+                name={name}
+                id={id}
+                className={`row-start-1 row-end-5 col-start-1 w-full rounded-2xl cursor-pointer appearance-none border-green-500 ${isChecked ? 'border-2' : 'group-hover:border-2'}`}
+                checked={isChecked}
+                onChange={handleOnClick}
+            />
+            <label htmlFor={id} className="flex flex-col items-center justify-center w-full col-start-1 row-start-2 row-end-4 cursor-pointer">
+                <Icon icon={icon} width="64" height="64" className="text-black transition-border p-2 duration-200 ease-in-out" />
                 <h6 className="font-poppins text-sm font-normal text-gray-600 uppercase">{name}</h6>
             </label>
-
         </div>
     );
 }
